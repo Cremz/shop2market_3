0221e7938446db717b181b6eccf0bd310e7cd428
Emanuel Comsa <cremzinc@gmail.com>
2015-08-11 23:53:12 +0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shop2market3::Application.config.secret_key_base = 'd90641208c8ab40be4a127bd04956d3d03522a1cbb26383120020c8e41ed9ac64dbf67ad5221773bced6a7a82709f4b8233be56300e07c88aa4b038f28f6e692'
