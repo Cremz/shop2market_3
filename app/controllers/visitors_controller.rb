2129758c40665427d7d085382a8885f97250c774
Emanuel Comsa <cremzinc@gmail.com>
2015-08-12 00:24:19 +0300

    Initial convertor.

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,2 +1,11 @@
 class VisitorsController < ApplicationController
+  def index
+    if params[:celsius]
+      params[:fahrenheit] = Convertor.from_c_to_f(params[:celsius].to_i)
+      return
+    end
+    if params[:fahrenheit]
+      params[:celsius] = Convertor.from_f_to_c(params[:fahrenheit].to_i)
+    end
+  end
 end
